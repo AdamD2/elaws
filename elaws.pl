f4cb8a5ed19121bc7462a10cb7db84300b9ac099
AdamD2 <adam_douglas2@hotmail.com>
2017-06-02 22:53:23 +1000

    Completed the base functionality of the program.

--- elaws.pl
+++ elaws.pl
@@ -7,4 +7,29 @@
 # License: GNU GPL v3
 #
 
+$url = "https://en.wikipedia.org/wiki/List_of_eponymous_laws";
 
+main();
+
+sub main {
+    open F, "wget -q -O- $url|" or die "Could not access the source.";
+    my @laws;
+
+    # Fetch each law and add it as a string into an array
+    while ($line = <F>) {
+        if ($line =~ /<li>(.*)<\/li>/) {
+            $line =~ s/<[^>]*>//g;
+
+            push @laws, $line;
+        }
+    }
+
+    # Check that there is at least one law to choose from
+    if (@laws > 0) {
+        # Generate a random number
+        my $number = int(rand(@laws));
+
+        # Print the law associated with that number
+        print $laws[$number];
+    }
+}
