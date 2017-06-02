9309c3485446ec5f99d69d2337d5f6194c2f970a
AdamD2 <adam_douglas2@hotmail.com>
2017-06-02 22:07:01 +1000

    Added info to the README and program header.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # elaws
+
+A simple terminal based program to fetch and display an eponymous law
+https://en.wikipedia.org/wiki/List_of_eponymous_laws
---
+++ elaws.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl -w
+#
+# Elaws - A simple terminal based program to fetch and display an eponymous law
+# https://en.wikipedia.org/wiki/List_of_eponymous_laws
+#
+# Author: Adam Douglas <adam_douglas2@hotmail.com>
+# License: GNU GPL v3
+#
+
+
